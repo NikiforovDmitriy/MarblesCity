ee824700a1d6c701044b94d78f8d5005c2cad268
NikiforovDmitriy <nikiforov.dmitriy@gmail.com>
2023-11-23 04:30:08 +0600

    geckos to http

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const app = express()
 const server = http.createServer(app)
 const port = 1444
 const geckos = require('@geckos.io/server/cjs/index').default
-const io = geckos()
+const io = geckos({ cors: { allowAuthorization: true } })
 
 io.addServer(server)
 
